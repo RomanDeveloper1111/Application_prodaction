9e3015c53895f8b0779def55019a8e6e9579015f
RomanDeveloper1111 <ignatenko-roma@list.ru>
2021-09-07 11:42:50 +0300

    new

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-k@n*4=4^(fgo9y6n2ujf93-u4$r*gkojxb8zw%gpjt#02%*@mn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '77.223.97.77']
+ALLOWED_HOSTS = []
 
 
 # Application definition
